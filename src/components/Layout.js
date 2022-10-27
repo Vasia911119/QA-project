5aa5c4ed05acf87a12980f51da6ba7e0d19b0be8
igorro79 <griniov.igor@gmail.com>
2022-10-27 23:44:11 +0300

    home page rendering

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
-import 'fontsource-inter'
-import React from 'react'
-import PropTypes from 'prop-types'
+import 'fontsource-inter';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Navbar from '../components/Navbar'
 
@@ -9,23 +9,23 @@ import PropTypes from 'prop-types'
 
 const Layout = ({ children }) => {
   return (
-    <p>hello</p>
-    // <section className="mx-auto relative antialiased max-sm:max-w-[480px] sm:w-[480px] md:w-[768px] xl:w-[1280px]">
-    //   <div className="md:w-[48px] xl:w-[348px] bg-blue-950 text-stone-400 fixed ">
-    //     <Header />
-    //     <Navbar />
-    //     <Footer />
-    //   </div>
+    // <p>hello</p>
+    <section className="max-sm:max-w-[480px] relative mx-auto antialiased sm:w-[480px] md:w-[768px] xl:w-[1280px]">
+      {/* <div className="md:w-[48px] xl:w-[348px] bg-blue-950 text-stone-400 fixed ">
+         <Header />
+         <Navbar />
+         <Footer />
+       </div> */}
 
-    //   <main className="">
-    //     <div className="md:ml-[48px] xl:ml-[348px]">{children}</div>
-    //   </main>
-    // </section>
-  )
-}
+      <main className="">
+        <div className="md:ml-[48px] xl:ml-[348px]">{children}</div>
+      </main>
+    </section>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,48 +12,66 @@ export default function HomePage({ data }) {
   const { edges: nodes } = data.allMarkdownRemark;
   const { i18n } = useTranslation();
 
-  // Костыль от Максима для украинского по умолчанию
-  // React.useEffect(() => {
-  //   const initialLang = window.localStorage.getItem('gatsby-i18next-language')
-  //   const visit = window.localStorage.getItem('visit')
+  // const pagesCollection = nodes.forEach(item => {
+  //   console.log(item.node.frontmatter.pages_chapter_item);
+  // });
+  const pagesCollection = nodes.find(item => {
+    if (item.node.frontmatter.pages_chapter_item)
+      return item.node.frontmatter.pages_chapter_item;
+  });
 
-  //   if (initialLang === 'ru' && !visit) {
-  //     window.localStorage.setItem('gatsby-i18next-language', 'uk')
-  //     window.localStorage.setItem('visit', 'true')
-  //     navigate('/uk')
-  //   }
-  // }, [])
+  const { pages_chapter_item: pagesCollectionList } =
+    pagesCollection.node.frontmatter;
+
+  const sortedPageChapters = pagesCollectionList.sort(
+    (a, b) => a.chapter_range - b.chapter_range
+  );
+  const homePageChapterName = sortedPageChapters[0].pages_chapter_name;
+
+  console.log('homePageChapterName', homePageChapterName);
+  console.log(nodes);
+
+  nodes.forEach(element => {
+    console.log(element);
+  });
+  console.log(Array.isArray(nodes));
+  const homePageNodes = nodes.filter(
+    i => i.node.frontmatter.page_chapter_name === homePageChapterName
+  );
+
+  console.log(homePageNodes);
 
   return (
-    <p>hello</p>
     // не обгорнуто в компонент Layout так як використовується плагін gatsby-plugin-layout
 
-    //     nodes &&
-    //     nodes.map(({ node: component }) => {
-    //       if (component.frontmatter.language === i18n.language) {
-    //         return (
-    //           <div
-    //             className="md:w-[608px] mx-auto pt-[32px] dark:bg-slate-300"
-    //             key={component.id}
-    //           >
-    //             <Breadcrumb title={component.frontmatter.title} />
-    //             <div className="space-y-4 text-left">
-    //               <h1 className="text-3xl leading-12 text-gray-800 lg:text-4xl lg:leading-14 mb-2">
-    //                 {component.frontmatter.title}
-    //               </h1>
-    //             </div>
-    //             <HTMLContent
-    //               className="prose max-w-none"
-    //               content={component.html}
-    //             />
-    //             <ButtonsNavigate />
-    //           </div>
-    //         )
-    //       }
-    //     })
+    homePageNodes &&
+    homePageNodes.map(({ node: component }) => {
+      console.log(component);
+      if (component.frontmatter.language === 'uk') {
+        console.log(component);
+        return (
+          <div
+            className="mx-auto pt-[32px] dark:bg-slate-300 md:w-[608px]"
+            // key={component.id}
+          >
+            <Breadcrumb title={component.frontmatter.title} />
+            <div className="space-y-4 text-left">
+              <h1 className="leading-12 lg:text-4xl lg:leading-14 mb-2 text-3xl text-gray-800">
+                {component.frontmatter.title}
+              </h1>
+            </div>
+            <HTMLContent
+              className="prose max-w-none"
+              content={component.html}
+            />
+            {/* <ButtonsNavigate /> */}
+          </div>
+        );
+      }
+    })
   );
 }
-
+//
 // HomePage.propTypes = {
 //   data: PropTypes.shape({
 //     markdownRemark: PropTypes.object,
@@ -62,7 +80,7 @@ export default function HomePage({ data }) {
 
 // НОВЫЙ QUERY
 // export const pageQuery = graphql`
-//   query ($language: String!) {
+// query ($language: String!) {
 //     locales: allLocale(filter: { language: { eq: $language } }) {
 //       edges {
 //         node {
@@ -72,61 +90,73 @@ export default function HomePage({ data }) {
 //         }
 //       }
 //     }
-//   allMarkdownRemark(filter: {frontmatter: {language: {eq: $language}}}) {
-//     nodes {
-//       frontmatter {
-//         language
-//         chapter_title
-//         link_chapter_name
-//         link_chapter_title
-//         links_chapter
-//         page_chapter
-//         page_chapter_name
-//         page_chapter_title
-//         page_range
-//         page_title
-//         links_items {
-//           link_title
-//           url_adress
+//     allMarkdownRemark(
+//       filter: { frontmatter: { language: { eq: $language } } }
+//     ) {
+//       nodes {
+//         frontmatter {
+//           language
+//           chapter_title
+//           link_chapter_name
+//           link_chapter_title
+//           links_chapter
+//           page_chapter
+//           page_chapter_name
+//           page_chapter_title
+//           page_range
+//           page_title
+//           links_items {
+//             link_title
+//             url_adress
+//           }
 //         }
+//         fields {
+//           slug
+//         }
+//         html
 //       }
-//       fields {
-//         slug
-//       }
-//       html
 //     }
 //   }
-//  }
-// `
+// `;
 
 // ЧИСТЫЙ ЗАПРОС С НОВОЙ СТРУКТУРОЙ С ЗАХАРДКОЖЕННЫМ ЯЗЫКОМ ДЛЯ ПРОВЕРКИ РАБОТЫ
-// {
-//   allMarkdownRemark(filter: {frontmatter: {language: {eq: "uk"}}}) {
-//     nodes {
-//       frontmatter {
-//         language
-//         chapter_title
-//         link_chapter_name
-//         link_chapter_title
-//         links_chapter
-//         page_chapter
-//         page_chapter_name
-//         page_chapter_title
-//         page_range
-//         page_title
-//         links_items {
-//           link_title
-//           url_adress
-//         }
-//       }
-//       fields {
-//         slug
-//       }
-//       html
-//     }
-//   }
-// }
-
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            language
+            chapter_title
+            link_chapter_name
+            link_chapter_title
+            links_chapter
+            page_chapter
+            page_chapter_name
+            page_chapter_title
+            page_range
+            links_items {
+              link_title
+              url_adress
+            }
+            title
+            page_title
+            pages_chapter_item {
+              chapter_range
+              pages_chapter_name
+            }
+            pages {
+              body
+              title
+            }
+          }
+          html
+          id
+        }
+      }
+    }
+  }
+`;
 // ЗАПРОС СО СТАРОЙ СТРУКТУРОЙ
 // export const pageQuery = graphql`
 //   query ($language: String!) {
