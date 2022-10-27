a67d29fbe4ba52a9fb1894289a0836f547bb1e98
Den <52295780+josserden@users.noreply.github.com>
2022-10-27 11:11:51 +0300

    fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,33 +1,43 @@
-const React = require('react')
+const React = require('react');
 
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
       key="darkmode"
       dangerouslySetInnerHTML={{
-        __html: `(function() {  
+        __html: `(function() {
             function setTheme(theme) {
               window.__theme = theme;
+
               if (theme === 'dark') {
                 document.documentElement.className = 'dark';
               } else {
-                document.documentElement.className = '';
+                document.documentElement.className = 'light';
               }
             };
+
             window.__setPreferredTheme = function(theme) {
               setTheme(theme);
+
               try {
                 localStorage.setItem('color-theme', theme);
-              } catch (e) {}
+              } catch (e) {
+                console.error(e);
+              }
             };
+
             let preferredTheme;
             try {
               preferredTheme = localStorage.getItem('color-theme');
-            } catch (e) {}
+            } catch (e) {
+                console.error(e);
+            }
+
             let darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
             setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
           })();`,
       }}
     />,
-  ])
-}
+  ]);
+};
--- src/components/Toggler/Toggler.js
+++ src/components/Toggler/Toggler.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import React, { useState, useEffect } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 // import { HiOutlineMoon, HiOutlineSun } from 'react-icons/hi'
 // import Home from '../images/home.inline.svg'
 // import Icon from '../images/Icon.inline.svg'
-import * as s from './Toggler.module.css'
+import * as s from './Toggler.module.css';
 
 const ToggleMode = () => {
-  let websiteTheme
-  if (typeof window !== `undefined`) {
-    websiteTheme = window.__theme
-    console.log('window.__theme', window.__theme)
-  }
-  useEffect(() => {
-    setTheme(window.__theme)
-  }, [])
+  let websiteTheme = typeof window !== `undefined` ? window.__theme : null;
+  const [theme, setTheme] = useState(websiteTheme);
 
-  const [theme, setTheme] = useState(websiteTheme)
+  // if (typeof window !== `undefined`) {
+  //   websiteTheme = window.__theme;
+  //   console.log('window.__theme', window.__theme);
+  // }
 
   useEffect(() => {
-    console.log('наш useEffect', theme)
-  }, [theme])
+    setTheme(window.__theme);
+    console.log('window.__theme', window.__theme);
+    console.log(websiteTheme === 'dark');
+  }, [theme]);
+
+  // useEffect(() => {
+  //   console.log('наш useEffect', theme);
+  // }, [theme]);
 
   const ThemeToggle = () => {
-    window.__setPreferredTheme(websiteTheme === 'dark' ? 'light' : 'dark')
-    setTheme(websiteTheme === 'dark' ? 'light' : 'dark')
-    console.log('websiteTheme', websiteTheme)
-  }
+    window.__setPreferredTheme(websiteTheme === 'dark' ? 'light' : 'dark');
 
-  console.log('theme', theme)
+    setTheme(websiteTheme === 'dark' ? 'light' : 'dark');
+  };
 
   return (
     <button
@@ -43,8 +44,10 @@ const ToggleMode = () => {
           <p className={s.darkIcon}>Dark</p>
         </>
       )}
-    </button>
-  )
-}
 
-export default ToggleMode
+      {}
+    </button>
+  );
+};
+
+export default ToggleMode;
