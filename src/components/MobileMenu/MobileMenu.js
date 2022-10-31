5354d0fed1afdfbadd6c7667330fd2680f60aa69
igorro79 <griniov.igor@gmail.com>
2022-10-31 10:58:20 +0200

    mobile menu css module

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
-import Header from '../Header';
-import Footer from '../Footer';
+// import Header from '../Header';
+// import Footer from '../Footer';
+import * as s from './MobileMenu.module.css';
 
 function MobileMenu({ setMobileOpen, handleClose, mobileOpen }) {
   return (
-    <div className={mobileOpen ? 'mobileMemuOpened' : 'mobileMemuClosed'}>
+    <div className={mobileOpen ? s.mobileMemuOpened : s.mobileMemuClosed}>
       <Navbar
         setMobileOpen={setMobileOpen}
         mobileOpen={mobileOpen}
