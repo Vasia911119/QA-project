b19b064b66ea829b1a3fddfd3ea7175c617fb1cd
galiovets <m.galiovets@gmail.com>
2022-11-01 20:41:07 +0200

    translations added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,47 @@
 // import { GatsbySeo } from 'gatsby-plugin-next-seo';
 import React from 'react';
+import { graphql } from 'gatsby';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 import * as s from '../styles/404.module.css';
 import Layout from '../components/Layout';
 import { Link } from 'gatsby';
 import { BiChevronRight } from 'react-icons/bi';
 
-const NotFoundPage = () => (
-  <section className="relative h-screen w-screen bg-[#DBD9D9] ">
-    <div className="absolute top-1/3 left-1/3  text-center font-inter">
-      <h1 className="mb-10 smOnly:text-2xl md:text-3xl">
-        Упс! <br /> Щось пішло не по плану
-      </h1>
-      <p className="mb-24 text-lg">
-        Давай повернемось на головну сторінку та спробуємо ще раз
-      </p>
-      <Link
-        to="/"
-        className="mx-auto flex h-12 w-[257px] items-center justify-center rounded-[12px] bg-blue-600  text-white"
-      >
-        <span className="mr-4">На головну</span>
-        <BiChevronRight className={s.iconArrow} alt="home" />
-      </Link>
-    </div>
-  </section>
-);
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+  const { oops, title, text, button } = t('404', { returnObjects: true });
+
+  return (
+    <section className="relative h-screen w-screen bg-[#DBD9D9] ">
+      <div className="absolute top-1/3 left-1/3  text-center font-inter">
+        <h1 className="mb-10 smOnly:text-2xl md:text-3xl">
+          {oops} <br /> {title}
+        </h1>
+        <p className="mb-24 text-lg">{text}</p>
+        <Link
+          to="/"
+          className="mx-auto flex h-12 w-[257px] items-center justify-center rounded-[12px] bg-blue-600  text-white"
+        >
+          <span className="mr-4">{button}</span>
+          <BiChevronRight className={s.iconArrow} alt="home" />
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default NotFoundPage;
+
+export const notFoundQuery = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
