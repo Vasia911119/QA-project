82d7e21b28c635d3b25dcbe28fe88cb999fc4989
Vasyl Yakymets <Vasia911119@gmail.com>
2022-11-03 02:32:33 +0200

    fix-list-styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/templates/component/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     // MEDIA QUERIES
